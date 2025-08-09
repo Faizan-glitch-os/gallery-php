2ac43abcdfad1607ad892d344ab0cb48d3aee71d
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-08-09 15:57:29 +0500

    code optized

--- image.php
+++ image.php
@@ -5,12 +5,15 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
-<h1><?php echo $imageTitles[$_GET['img']] ?></h1>
-<img src="./images/<?php echo $_GET['img'] ?>" alt="<?php echo $imageTitles[$_GET['img']] ?>">
-<p>
-    <?php
-        echo $imageDescriptions[$_GET['img']]; 
-    ?>         
-</p>
+<?php
+    if(!empty($_GET['img']) && !empty($imageTitles[$_GET['img']])): ?>
+       <?php $img = e($_GET['img']); ?>
+       <h1><?php echo $imageTitles[$img] ?></h1>
+       <img src="./images/<?php echo $img ?>" alt="<?php echo $imageTitles[$img] ?>">
+       <p><?php echo $imageDescriptions[$img]; ?></p>
+<?php else: ?>
+    <p>Page not found</p>
+<?php endif; ?>
+
 <a href="<?php echo rawurlencode("gallery.php") ?>">Back to Gallery</a>
 <?php include './views/footer.php'; ?>
